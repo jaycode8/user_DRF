65693783062a0268b970299a5ec849a42654cd3a
jaycode8 <jamesmumo443@gmail.com>
2024-07-08 11:57:12 +0300

    updates

--- DRF/settings.py
+++ DRF/settings.py
@@ -81,6 +81,13 @@ WSGI_APPLICATION = 'DRF.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
